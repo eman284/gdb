ad0257781cea6ac2baba2326c58e1adf86a44257
Iman Hussien <a.ihussien@dev.thiqah.sa>
2022-02-13 12:48:46 +0200

    add databind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  data1 = 'One way binding';
-  data2 = 'Two way binding';
+  data1:string = 'One way binding';
+  data2 = 'hi 123';
+  d1= 'input 1';
+  d2= 'input 2';
+  d3= 'input 3';
   isActive = true;
 
   printName():void{
