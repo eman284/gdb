0c43e85d5ba9fe980e02d98defd4ede47c9f20b9
Iman Hussien <a.ihussien@dev.thiqah.sa>
2022-02-13 11:18:54 +0200

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firstApp';
+  isActive: boolean =true;
+  fruits: string[] =['banana', 'Apple'] 
 }
